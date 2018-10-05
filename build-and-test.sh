5b623fc369e1cb6ce6081c83e29b5f4913b3f15c
Julian Oes <julian@oes.ch>
2018-10-05 10:32:44 +0200

    Add script to run tests with gcc and clang

---
+++ build-and-test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+rm -rf build
+
+(mkdir -p build && cd build && CXX=g++ cmake .. && make -j8 && valgrind ./lockstep_scheduler_test)
+rm -rf build
+
+(mkdir -p build && cd build && CXX=clang++ cmake .. && make -j8 && valgrind ./lockstep_scheduler_test)
+rm -rf build
