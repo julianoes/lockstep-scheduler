e31d7cebce74fcb3d7dccccaaddb7dd1ee31d0f7
Julian Oes <julian@oes.ch>
2018-10-07 17:02:18 +0200

    Re-organize cmake folder structure

--- build-and-test.sh
+++ build-and-test.sh
@@ -4,8 +4,8 @@ set -e
 
 rm -rf build
 
-(mkdir -p build && cd build && CXX=g++ cmake .. && make -j8 && $@ ./lockstep_scheduler_test)
+(mkdir -p build && cd build && CXX=g++ cmake .. && make -j8 && $@ ./test/lockstep_scheduler_test)
 rm -rf build
 
-(mkdir -p build && cd build && CXX=clang++ cmake .. && make -j8 && $@ ./lockstep_scheduler_test)
+(mkdir -p build && cd build && CXX=clang++ cmake .. && make -j8 && $@ ./test/lockstep_scheduler_test)
 rm -rf build
